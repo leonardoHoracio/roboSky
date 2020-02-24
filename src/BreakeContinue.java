f5bafadf79a812975400fd6cb48a609b9090f3c3
Leonardo <Leonardo@Leonardo-PC>
2020-02-24 14:46:25 -0300

    testando

---
+++ src/BreakeContinue.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BreakeContinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+
+
+		/*
+		 * System.out.println("Entre com um número"); int num=in.nextInt();
+		 * 
+		 * System.out.println("Entre com o limite"); int max=in.nextInt();
+		 * 
+		 * 
+		 * for(int i=num;i<=max;i++){
+		 * 
+		 * System.out.println(i); if(i%7==0){
+		 * System.out.println("Divisel por 7 "+i); break; }
+		 * 
+		 * }
+		 */
+
+	}
+
+}
